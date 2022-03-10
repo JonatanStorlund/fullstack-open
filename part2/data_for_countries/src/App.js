5543e08f4582f515551459da46d6d966d34068d1
Jonatan Storlund <storlundjonatan@gmail.com>
2022-03-10 17:21:50 +0200

    2.14 done

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -1,7 +1,20 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const api_key = process.env.REACT_APP_API_KEY
+
 const FullCountryInfo = ({data}) => {
+  const [weatherData, setWeatherData] = useState()
+  
+  useEffect(() => {
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${data.capital[0]}&appid=${api_key}&units=metric`)
+    .then(response => {
+      setWeatherData(response.data)
+    })
+  }, [data.capital])
+
+  if (!weatherData) return <h1>Loading...</h1>
+
   return (
       <>
         <h1>{data.name.common}</h1>
@@ -12,6 +25,11 @@ const FullCountryInfo = ({data}) => {
           {Object.values(data.languages).map(language => <li key={language}>{language}</li>)}
         </ul>
         <p>{data.flag}</p>
+        <h2>Weather in {data.capital[0]}</h2>
+        <p>temperature {weatherData.main.temp}</p>
+        <p>wind {weatherData.wind.speed} m/s</p>
+        {console.log(weatherData)}
+        <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="weathericon" />
     </>
   )
 }
@@ -22,7 +40,7 @@ const ToggleFullInfo = ({data}) => {
   return (
     <>
       {isShow ? <FullCountryInfo data={data} /> : <h1>{data.name.common}</h1>}
-      <button onClick={() => setIsShow(!isShow)}>{isShow ? <h1>hide</h1> : <h1>show</h1>}</button>
+      <button onClick={() => setIsShow(!isShow)}>{isShow ? 'hide' : 'show'}</button>
     </>
   )
 }
@@ -45,17 +63,17 @@ const Results = ({apiData, filter}) => {
   )
 }
 
-function App() {
+const App = () => {
   const [apiData, setApiData] = useState([])
   const [filterValue, setFilterValue] = useState('')
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all')
-      .then(response => {
-        setApiData(response.data)
-      })
-    }, [])
-    
+    .then(response => {
+      setApiData(response.data)
+    })    
+  }, [])
+  
   const handleFilter = (event) => {
     setFilterValue(event.target.value.toLowerCase())
   }
